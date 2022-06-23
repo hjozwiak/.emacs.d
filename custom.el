8edf038cdcafa076aa9284f3e0358431e7668b15
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-06-23 14:41:15 -0400

    Tweaks to custom file.
    This should probably be removed soon, since it really doesn't serve
    mmuch of a purpose.

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-tabnine company helpful macrostep no-littering dash f s leaf-defaults leaf-manager blackout hydra leaf-keywords leaf)))
+   '(magit-gitflow forge magit evil-collection evil undo-tree general no-littering blackout hydra leaf-keywords leaf)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
