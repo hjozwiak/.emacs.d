fa767968747ecb1eb524a3788f6595c37c31ff4c
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-06-24 17:40:44 -0400

    Project module.

---
+++ modules/projects.el
@@ -0,0 +1,35 @@
+;;; projects.el --- Configuration for projects.      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Hunter Jozwiak
+
+;; Author: Hunter Jozwiak <hunter.t.joz@gmail.com>
+;; Keywords: project, builtins
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; We'll use the Emacs built-in project manager for keeping track of our projects for now.
+;;; Code:
+
+
+(leaf project
+  :tags "builtin" "project"
+  :doc "A project manager for Emacs.") 
+
+
+
+
+(provide 'projects)
+;;; projects.el ends here
