483d0a9a52cf45ad06534ec2b4a4ca181eb50a9a
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-06-27 23:09:11 -0400

    Add a keyword to the header.

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022  Hunter Jozwiak
 
 ;; Author: Hunter Jozwiak <hunter.t.joz@gmail.com>
-;; Keywords: 
+;; Keywords: config
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
